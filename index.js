a66610d31897fe4b5ef17e66597490ac9071425e
Elijah Nyasiando <143831019+mgkEdits@users.noreply.github.com>
2023-09-23 20:56:04 +0300

    completed messages

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Code your solutions in this file
+function writeCards(names, eventName) {
+    const thankYouMessages = [];
+    for (let i = 0; i < names.length; i++) {
+      thankYouMessages.push(`Thank you, ${names[i]}, for the wonderful ${eventName} gift!`);
+    }
+    return thankYouMessages;
+  }
+
+  function countDown(number) {
+  for (let i = number; i >= 0; i--) {
+    console.log(i);
+  }
+}
